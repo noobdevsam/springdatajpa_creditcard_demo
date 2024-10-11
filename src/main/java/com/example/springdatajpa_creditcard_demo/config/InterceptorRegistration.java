e2a83875c38f5beaf74bc77ee7abd8a52b5bb33e
mrdevsam <mdshoykotislam05@gmail.com>
2024-10-11 21:36:54 +0600

    created hibernate interceptor. failed test is not fixed yet.

---
+++ src/main/java/com/example/springdatajpa_creditcard_demo/config/InterceptorRegistration.java
@@ -0,0 +1,24 @@
+package com.example.springdatajpa_creditcard_demo.config;
+
+import java.util.Map;
+
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.springdatajpa_creditcard_demo.interceptors.EncryptionInterceptor;
+
+@Configuration
+public class InterceptorRegistration implements HibernatePropertiesCustomizer{
+
+    private final EncryptionInterceptor interceptor;
+
+    public InterceptorRegistration(EncryptionInterceptor interceptor) {
+        this.interceptor = interceptor;
+    }
+
+    @Override
+    public void customize(Map<String, Object> hibernateProperties) {
+        hibernateProperties.put("hibernate.session_factory.interceptor", interceptor);
+    }
+    
+}
---
+++ src/main/java/com/example/springdatajpa_creditcard_demo/interceptors/EncryptionInterceptor.java
@@ -0,0 +1,36 @@
+package com.example.springdatajpa_creditcard_demo.interceptors;
+
+import java.io.Serializable;
+
+import org.hibernate.CallbackException;
+import org.hibernate.EmptyInterceptor;
+import org.hibernate.type.Type;
+import org.springframework.stereotype.Component;
+
+@SuppressWarnings("deprecation")
+@Component
+public class EncryptionInterceptor extends EmptyInterceptor{
+
+    @Override
+    public boolean onFlushDirty(Object entity, Serializable id, Object[] currentState, Object[] previousState,
+            String[] propertyNames, Type[] types) throws CallbackException {
+                System.out.println("I am in onFlushDirty");
+        return super.onFlushDirty(entity, id, currentState, previousState, propertyNames, types);
+    }
+
+    @Override
+    public boolean onLoad(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types)
+            throws CallbackException {
+                System.out.println("I am in onLoad");
+        return super.onLoad(entity, id, state, propertyNames, types);
+    }
+
+    @Override
+    public boolean onSave(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types)
+            throws CallbackException {
+                System.out.println("I am in onSave");
+        return super.onSave(entity, id, state, propertyNames, types);
+    }
+    
+    
+}
